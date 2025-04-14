03ff463ba414602d11de8a9fc74515ccf9346cd6
msalah2024 <ms1912733@qu.edu.qa>
2025-04-14 15:10:40 +0300

    tabs navigation on settings

--- components/SettingsHeader.tsx
+++ components/SettingsHeader.tsx
@@ -1,13 +1,24 @@
+"use client"
 import React from 'react'
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { usePathname, useRouter } from 'next/navigation'
 
 export default function SettingsHeader() {
+    const router = useRouter()
+    const pathname = usePathname()
+
+    const activeTab = pathname.includes('account') ? 'account' : 'profile'
+
+    const handleTabChange = (value: string) => {
+        router.push(`/protected/settings/${value}`);
+    }
+
     return (
         <div className='mb-3'>
             <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 text-primary-foreground-muted">
                 Settings
             </h2>
-            <Tabs defaultValue="account">
+            <Tabs defaultValue="account" value={activeTab} onValueChange={handleTabChange}>
                 <TabsList className='h-fit bg-background gap-6 flex-wrap'>
                     <TabsTrigger value="account"
                         className='px-4a border-b-background py-2 text-primary-foreground-muted rounded-none 
