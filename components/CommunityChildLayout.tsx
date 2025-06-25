21b1a3f8fced3eb9b082f97856275987ee8f13e0
msalah2024 <ms1912733@qu.edu.qa>
2025-06-25 22:00:37 +0300

    fixed max width for comments page

--- components/CommunityChildLayout.tsx
+++ components/CommunityChildLayout.tsx
@@ -3,14 +3,19 @@ import React from 'react'
 import CommunityHeader from './CommunityHeader'
 import CommunityRightSide from './CommunityRightSide'
 import { useView } from '@/app/context/ViewContext'
+import { usePathname } from 'next/navigation'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function CommunityChildLayout({ children }: any) {
   const { view } = useView()
+  const pathname = usePathname()
+
+  const isPostPage = pathname.includes("/comments")
+  const shouldUseMaxWidth = view === 'Card' || isPostPage
 
   return (
     <div className='lg:mx-8 flex justify-center'>
-      <div className={`w-full ${view === 'Card' ? "max-w-6xl" : "w-full"} `}>
+      <div className={`w-full ${shouldUseMaxWidth ? "max-w-6xl" : "w-full"}`}>
         <CommunityHeader />
         <div className='flex gap-4'>
           <div className='w-full mt-4 mx-4'>
